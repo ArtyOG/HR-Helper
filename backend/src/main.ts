0520a82badcea413ef07b5406924198f57a6b116
ArtyOG <yangvitou7001@gmail.com>
2026-09-20 12:13:16 +0700

    fix(backend): run precompiled dist/main.js in Dockerfile to prevent JavaScript heap out of memory and bind to 0.0.0.0

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -28,7 +28,7 @@ async function bootstrap() {
   );
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup("docs", app, document);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 
 bootstrap();
